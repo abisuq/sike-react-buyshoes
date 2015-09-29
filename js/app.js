ee001ba0950f6521f2206515d7229754578e1c88
crs0910 <gettaiwan@qq.com>
2015-09-30 00:14:05 +0800

    layout

--- js/app.js
+++ js/app.js
@@ -1,3 +1,9 @@
 window.onload = function() {
-  console.log("page loaded");
+  makeCartScrollNicely();
+}
+function makeCartScrollNicely() {
+  var cart = document.querySelector('.site__right-sidebar .cart');
+  var products = document.querySelector('.site__content .products');
+  Ps.initialize(cart);
+  Ps.initialize(products);
 }
