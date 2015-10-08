fe69d9903ca7a994d047939e98585ed2ef8fa91a
qusiba <chenrunshan@outlook.com>
2015-10-09 01:01:49 +0800

    finish

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 window.onload = function() {
   makeCartScrollNicely();
+  var $toggle = document.querySelector(".site__right-sidebar-toggle");
+  $toggle.addEventListener("click",function() {
+    document.body.classList.toggle("js-show-right-sidebar");
+  });
 }
 function makeCartScrollNicely() {
   var cart = document.querySelector('.site__right-sidebar .cart');
-  var products = document.querySelector('.site__content .products');
   Ps.initialize(cart);
-  Ps.initialize(products);
 }
